f09dd945aeaf7560015ff88af883ca1ac91ae6a8
Venugopal Ravu <venugopalarao.r@gmail.com>
2025-12-05 10:32:01 +0530

    Added LLM callback handler

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from typing import List, Union
+
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
 from langchain_classic.agents.output_parsers import ReActSingleInputOutputParser
-from langchain_core.tools import Tool,render_text_description, tool
 from langchain_core.agents import AgentAction, AgentFinish
+from langchain_classic.agents.format_scratchpad import format_log_to_str
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool, render_text_description, tool
 from langchain_openai import ChatOpenAI
+
+from callbacks import AgentCallbackHandler
 
 load_dotenv()
 
@@ -17,12 +21,12 @@ def get_text_length(text: str) -> int:
     text = text.strip("'\n").strip("''")
     return len(text)
 
-def find_tool_by_name(tools: List[Tool], tool_name:str) -> Tool:
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
     for tool in tools:
-        if tool.name== tool_name:
+        if tool.name == tool_name:
             return tool
     raise ValueError(f"Tool with {tool_name} not found")
-
 
 
 def main():
@@ -47,37 +51,40 @@ def main():
     Begin!
 
     Question: {input}
-    Thought:
+    Thought: {agent_scratchpad}
     """
     prompt = PromptTemplate.from_template(template=template).partial(
         tools=render_text_description(tools),
         tool_names=", ".join(t.name for t in tools),
     )
-    llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"])
-    intermediate_steps=[]
-    agent=(
-        {
-            "input":lambda x: x["input"]
-        }
-        | prompt
-        | llm
-        | ReActSingleInputOutputParser()
+    llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"], callbacks=[AgentCallbackHandler()])
+    intermediate_steps = []
+    agent = (
+        {"input": lambda x: x["input"],
+         "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
+         } 
+        | prompt | llm | ReActSingleInputOutputParser()
     )
+    agent_step = ""
+    while not isinstance(agent_step, AgentFinish):
 
-    agent_step: Union[AgentAction,AgentFinish]= agent.invoke({
-        "input" : "What is the length of DOG in characters"
-    }
-    )
-    print(agent_step)
-    if isinstance(agent_step,AgentAction):
-        tool_name=agent_step.tool
-        tool_to_use=find_tool_by_name(tools,tool_name)
-        tool_input=agent_step.tool_input
-        observation=tool_to_use.func(str(tool_input))
-        print(f"{observation=}")
+        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+            {
+                "input": "What is the length of DOG in characters",
+                "agent_scratchpad": intermediate_steps
+            }
+        )
+        print(agent_step)
+        if isinstance(agent_step, AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input
+            observation = tool_to_use.func(str(tool_input))
+            print(f"{observation=}")
+            intermediate_steps.append((agent_step, str(observation)))
 
-
-
+    if isinstance(agent_step, AgentFinish):
+        print(agent_step.return_values)
 
 
 if __name__ == "__main__":
