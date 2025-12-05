bdd26e1d4283cd554383f224dd0992f0d4592efd
Venugopal Ravu <venugopalarao.r@gmail.com>
2025-12-05 10:14:45 +0530

    Add ReAct loop and CallbackHandler for agent execution Implement the full ReAct reasoning loop with intermediate steps tracking and agent scratchpad formatting. Add AgentCallbackHandler to log LLM prompts and responses for debugging. The agent now iteratively reasons, acts, and observes until reaching a final answer.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from typing import List, Union
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain_core.tools import render_text_description, tool
+from langchain_classic.agents.output_parsers import ReActSingleInputOutputParser
+from langchain_core.tools import Tool,render_text_description, tool
+from langchain_core.agents import AgentAction, AgentFinish
 from langchain_openai import ChatOpenAI
 
 load_dotenv()
@@ -13,6 +16,13 @@ def get_text_length(text: str) -> int:
     # Stripping away non alphabetic characters in case
     text = text.strip("'\n").strip("''")
     return len(text)
+
+def find_tool_by_name(tools: List[Tool], tool_name:str) -> Tool:
+    for tool in tools:
+        if tool.name== tool_name:
+            return tool
+    raise ValueError(f"Tool with {tool_name} not found")
+
 
 
 def main():
@@ -37,13 +47,37 @@ def main():
     Begin!
 
     Question: {input}
-    Thought:{agent_scratchpad}
+    Thought:
     """
     prompt = PromptTemplate.from_template(template=template).partial(
         tools=render_text_description(tools),
         tool_names=", ".join(t.name for t in tools),
     )
     llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"])
+    intermediate_steps=[]
+    agent=(
+        {
+            "input":lambda x: x["input"]
+        }
+        | prompt
+        | llm
+        | ReActSingleInputOutputParser()
+    )
+
+    agent_step: Union[AgentAction,AgentFinish]= agent.invoke({
+        "input" : "What is the length of DOG in characters"
+    }
+    )
+    print(agent_step)
+    if isinstance(agent_step,AgentAction):
+        tool_name=agent_step.tool
+        tool_to_use=find_tool_by_name(tools,tool_name)
+        tool_input=agent_step.tool_input
+        observation=tool_to_use.func(str(tool_input))
+        print(f"{observation=}")
+
+
+
 
 
 if __name__ == "__main__":
